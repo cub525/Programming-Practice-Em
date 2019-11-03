c6e9bf847a43dcff64508c70e9702a86fc035fc9
tilaloz <marshall.hart@gmail.com>
2019-11-03 09:34:45 -0700

    Most recent changes
    Mo

--- python_challenge.py
+++ python_challenge.py
@@ -7,16 +7,42 @@ Created on Mon Oct 28 15:06:45 2019
 from collections import Counter
 import re
 import urllib3
+from zipfile import ZipFile
 
+def channel_solution():
+    url = 'http://www.pythonchallenge.com/pc/def/channel.zip'
+    http = urllib3.PoolManager()
+    r = http.request('GET',url)
+    with open('channel.zip','wb') as out:
+         
+    print(z)
+    
+    
+     
 def translate_string(n,string):
     alphabet_ord = [i for i in range(97,123)]
     alphabet_str = [chr(i) for i in range(97,123)]
     #Shift list and make dictionary
     renum_ord = [alphabet_str[i+(n-26)] for i in range(len(alphabet_str))]
     dict1 = {alphabet_ord[i]:renum_ord[i] for i in range(len(alphabet_str))}
+    # A totally equivalent line to the above (a little more pythonic, I think)
+    dict2 = {number:letter for (number,letter) in zip(alphabet_ord,renum_ord)}
     test = str(string).translate(dict1)
     print(test)
 
+def translate_string_dad(n,string):
+    """
+    Dad's version of Emmett's translate_string, no idea if it is actually better
+    or cleaner.. just I had to work through it.
+    -- Only works for lowercase... uppercase will remain unchanged
+    """
+    alphabet_str = [chr(i) for i in range(ord('a'),ord('z')+1)]
+    alphabet_str2 = alphabet_str[:]
+    #Shift list and maketrans
+    for i in range(n): alphabet_str2.append(alphabet_str2.pop(0)) 
+    transrule = str.maketrans(''.join(alphabet_str),''.join(alphabet_str2))
+    print(string.translate(transrule))
+    
 def freq_in_str(string):
     chars = Counter([i for i in string])
     fq_chars = chars.most_common()
@@ -31,22 +57,29 @@ def type_in_string(string):
 
 def web_lookup(initial_url):
     dat1 = 0
+    http = urllib3.PoolManager()
     try:
-        while 1:
-            http = urllib3.PoolManager()
+        while 1:          
             r = http.request('GET',initial_url)
             datr = r.data.decode("utf-8")
             try:
+                print(datr)
                 dat = int(''.join([i for i in datr if i.isdigit()]))
+                print(dat)
                 initial_url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing={0}'.format(dat)
+                print(initial_url)
+            
             except ValueError:
                 print(datr)
                 print(dat1)
+                break
             dat1 = dat
             
     except KeyboardInterrupt:
         pass
 
 if __name__ == '__main__':
-#    translate_string(2,'map')
-    web_lookup('http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=8022')
+#    translate_string_dad(2,'AaBbcdefghijklmnopqrstuvwxyz')
+#    translate_string(2,'AaBbcdefghijklmnopqrstuvwxyz')
+#    web_lookup('http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=826836357923542345463246345623452345')
+    channel_solution()
