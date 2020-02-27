d83fdbc583f7ad2001464595050d6f9fc109cbac
cub525 <emmett@hartlings.net>
2020-02-27 15:32:46 -0700

    flat code to def structure
    Added a if __name... statement

--- RC_Lab.py
+++ RC_Lab.py
@@ -2,17 +2,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 
-data = np.genfromtxt('rc_lab_1_datonly.csv', dtype = 'float',
-                     names = True,delimiter=',')
+file = 'rc_lab_1_datonly.csv'
 C = .1 #Farad
 r = 973 #ohm
+def solve_lab(data,r,C):
+    data = np.genfromtxt(data, dtype = 'float',
+                         names = True,delimiter=',')
+    lnvolts = [np.log(data['Voltage'][0]/float(i)) for i in data['Voltage']]
+    slope, intercept, r_value, p_value, std_err = stats.linregress(data['Time'], lnvolts)
+    percenterror = round(100*abs(r*C-(slope**-1))/(r*C),3)
+    rsqrd = round(r_value**2, 3)
+    
+    plt.plot(data['Time'],lnvolts,marker='.',linestyle='none',label = 'Scatter plot of data')
+    plt.plot(data['Time'], np.poly1d(np.polyfit(data['Time'], lnvolts, 1))
+    (data['Time']),label='Best fit line\nr^2={0}\npercent error = {1}%'.format(rsqrd,percenterror))
+    plt.plot(0,0,color='w',label = 'r = {0}\nC = {1}'.format(r,C))
+    plt.legend()
+    plt.show()
 
-lnvolts = [np.log(data['Voltage'][0]/i) for i in data['Voltage']]
-slope, intercept, r_value, p_value, std_err = stats.linregress(data['Time'], lnvolts)
-percenterror = round(100*abs(r*C-slope**-1)/r*C,3)
-rsqrd = x = round(r_value**2, 3)
-
-plt.plot(data['Time'],lnvolts,marker='.',linestyle='none',label = 'Scatter plot of data')
-bestfit = plt.plot(data['Time'], np.poly1d(np.polyfit(data['Time'], lnvolts, 1))
-(data['Time']),label='Best fit line \n r^2={0} \n percent error = {1}%'.format(rsqrd,percenterror))
-plt.legend()
+if __name__ == '__main__':
+    solve_lab(file,r,C)
+    solve_lab('RC_Lab_casey.csv',270,.025)
