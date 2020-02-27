3c43ca2378e3e01ac166d96fada7389f754316b6
cub525 <emmett@hartlings.net>
2020-02-27 08:12:23 -0700

    Added some txt files for refence

--- bioHomework.py
+++ bioHomework.py
@@ -8,49 +8,68 @@ Created on Mon Feb 17 19:43:38 2020
 # Python program to conver 
 # altered DNA to protein 
   
-inputfile ="DNA_sequence_original.txt" 
-f = open(inputfile, "r") 
-seq = f.read() 
-  
-seq = seq.replace("\n", "")  
-seq = seq.replace("\r", "") 
-  
-def translate(seq): 
-      
-    table = { 
-        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M', 
-        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T', 
-        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K', 
-        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',                  
-        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L', 
-        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P', 
-        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q', 
-        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R', 
-        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V', 
-        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A', 
-        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E', 
-        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G', 
-        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S', 
-        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L', 
-        'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_', 
-        'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W', 
+#inputfile ="DNA.txt" 
+#f = open(inputfile, "r") 
+#seq = f.read() 
+#  
+#seq = seq.replace("\n", "")  
+#seq = seq.replace("\r", "") 
+
+table = { 
+        'AUA': 'I', 'AUC': 'I', 'AUU': 'I', 'AUG': 'M',
+        'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACU': 'T',
+        'AAC': 'N', 'AAU': 'N', 'AAA': 'K', 'AAG': 'K',
+        'AGC': 'S', 'AGU': 'S', 'AGA': 'R', 'AGG': 'R',
+        'CUA': 'L', 'CUC': 'L', 'CUG': 'L', 'CUU': 'L',
+        'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCU': 'P',
+        'CAC': 'H', 'CAU': 'H', 'CAA': 'Q', 'CAG': 'Q',
+        'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGU': 'R',
+        'GUA': 'V', 'GUC': 'V', 'GUG': 'V', 'GUU': 'V',
+        'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCU': 'A',
+        'GAC': 'D', 'GAU': 'D', 'GAA': 'E', 'GAG': 'E',
+        'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGU': 'G',
+        'UCA': 'S', 'UCC': 'S', 'UCG': 'S', 'UCU': 'S',
+        'UUC': 'F', 'UUU': 'F', 'UUA': 'L', 'UUG': 'L',
+        'UAC': 'Y', 'UAU': 'Y', 'UAA': '_', 'UAG': '_',
+        'UGC': 'C', 'UGU': 'C', 'UGA': '_', 'UGG': 'W' 
     } 
+
+inv_table = {v: k for k, v in table.items()}
+
+def dnatomrna(seq):
+    scribe = seq.maketrans('ACTG','UGAC')
+    mrna = seq.translate(scribe)
+    return mrna
+
+def rnatodna(seq):
+    scribe = seq.maketrans('UGAC','ACTG')
+    mrna = seq.translate(scribe)
+    return mrna
+
+def mrnatotrna(seq):
+    scribe = seq.maketrans('ACUG','UGAC')
+    mrna = seq.translate(scribe)
+    return mrna
+
+def rnatoaa(seq):
     protein ="" 
     if len(seq)%3 == 0: 
         for i in range(0, len(seq), 3): 
             codon = seq[i:i + 3] 
             protein+= table[codon] 
     return protein 
-def read_seq(inputfile): 
-    with open(inputfile, "r") as f: 
-        seq = f.read() 
-    seq = seq.replace("\n", "") 
-    seq = seq.replace("\r", "") 
-    return seq 
-  
-prt = read_seq("amino_acid_sequence_original.txt") 
-dna = read_seq("DNA_sequence_original.txt") 
-  
-  
-p = translate(dna[20:935]) 
-p == prt  
+
+def dnatoaa(seq):
+    rna = dnatomrna(seq)
+    protein = rnatoaa(rna)
+    return protein
+
+
+def aatorna(aa):
+    rna = ''
+    for i in aa:
+        rna += inv_table[i]
+    return rna
+if __name__ == '__main__':
+    print(dnatoaa('TACCGCTCCGCCGTCGACAATACCACT'))
+ 
